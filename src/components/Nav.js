83f0371f83feccd19d11b6c2b7691ae389b1fa37
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-11-02 19:17:24 -0400

    redesign nav bar and add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import logo from '../assets/logo/fried-egg 1.svg';
+import bag from '../assets/logo/shopping-bag (1).png';
 
 function Nav() {
+
+    const [cartItems, setCartItems] = useState(0);
+
     return(
         <div className='nav-container'>
-            
+            <img src={logo} alt=""></img>
+
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                </ul>
+
+                <div className='shopping-cart'>
+                    <img src={bag} alt=""></img>
+                    <span>Cart: {cartItems}</span>
+                </div>
+            </div>
         </div>
     )
 }
